49cd6223e3eedeab171bf5b40bacc0d7d0150fe8
studenti <studenti@rosselli.loc>
2020-02-03 08:30:12 +0100

    Add project files.

---
+++ StruttureDati2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StruttureDati2
+{
+    class Program
+    {
+        private const string file = @"file.txt";
+        private const string file1 = @"file1.txt";
+        static void Main(string[] args)
+        {
+            HashSet<int> numeri = new HashSet<int>();
+            using (StreamReader rfile = new StreamReader(file))
+            {
+                string line;
+                while ((line = rfile.ReadLine()) != null)
+                {
+                    int n = int.Parse(line);
+                    numeri.Add(n);
+                }
+            }
+            using (StreamWriter swfile = new StreamWriter(file1, false))
+            {
+                foreach (int d in numeri)
+                swfile.WriteLine(d);
+            }
+        }
+    }
+}
